c6f8f1fed54fd3866b65c8563101804163434853
reutby <reutby321@gmail.com>
2020-12-29 13:03:00 +0200

    adding jumbotron components and global-style

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
-
+import React from "react";
+import Jumbotron from "./components/jumbotron";
+import JumboData from "./fixtures/jumbo.json";
+import "normalize.css";
 function App() {
   return (
-    <div className="App">
-  <p>Hello</p>
-    </div>
+      <Jumbotron.Container>
+      {JumboData.map(jumbo=>{
+        return (
+        <Jumbotron key={jumbo.id} direction={jumbo.direction} >
+          <Jumbotron.Pane>
+              <Jumbotron.Title>{jumbo.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{jumbo.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={jumbo.image} alt={jumbo.alt}/>
+          </Jumbotron.Pane>
+          </Jumbotron>)
+      })}
+
+      </Jumbotron.Container>
+    
   );
 }
 
