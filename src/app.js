e5a94e9bad39d5368f399dcfb5c0c723c8ccfde2
reutby <reutby321@gmail.com>
2021-01-03 18:26:10 +0200

    using costume hooks for checking aithentication and extract data from firebase db to the browser page

--- src/app.js
+++ src/app.js
@@ -1,18 +1,46 @@
 import React from "react";
 import * as ROUTES from "./constants/routes";
-import {Switch, Route, BrowserRouter} from "react-router-dom";
-import {Browse, Signin, Signup, Home} from "./pages"
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { BrowserRouter, Switch } from "react-router-dom";
+import { Browse, Signin, Signup, Home } from "./pages";
+import {useAuthListener} from "./hooks";
+
 export default function App() {
+
+  const { user } = useAuthListener();
+
   return (
     <BrowserRouter>
       <Switch>
-        <Route path={ROUTES.BROWSE} component = {Browse}/>
-        <Route path={ROUTES.SIGN_IN} component = {Signin}/>
-        <Route path={ROUTES.SIGN_UP} component = {Signup}/>
-        <Route path={ROUTES.HOME} exact component = {Home}/>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+
+        <IsUserRedirect
+          path={ROUTES.SIGN_IN}
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+        >
+          <Signin />
+        </IsUserRedirect>
+
+        <IsUserRedirect
+          path={ROUTES.SIGN_UP}
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+        >
+          <Signup />
+        </IsUserRedirect>
+
+        <IsUserRedirect
+          path={ROUTES.HOME}
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          exact
+        >
+          <Home />
+        </IsUserRedirect>
       </Switch>
     </BrowserRouter>
   );
 }
-
-
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,12 @@
 import React from "react";
-
+import {useContent} from "../hooks"
+import {selectionFilter} from "../utils";
 export default function Browse(){
+
+    const {series} = useContent("series");
+    const {films} = useContent("films");
+    
+    const slides = selectionFilter({series , films });
+    console.log(films,series,slides);
     return <p>Im in the browse page!</p>
 }
